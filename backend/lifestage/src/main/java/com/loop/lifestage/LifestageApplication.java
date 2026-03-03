f63ab579af189695bd656309f8fe98cfaa7c8264
MartinHatlem <martinfhatlem@gmail.com>
2026-03-03 10:10:52 +0100

    chore: add backend boilerplate with spring initializr
    Using springboot w. maven and adding spring web and dev tools dependencies

---
+++ backend/lifestage/src/main/java/com/loop/lifestage/LifestageApplication.java
@@ -0,0 +1,13 @@
+package com.loop.lifestage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifestageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifestageApplication.class, args);
+	}
+
+}
---
+++ backend/lifestage/src/test/java/com/loop/lifestage/LifestageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loop.lifestage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifestageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
