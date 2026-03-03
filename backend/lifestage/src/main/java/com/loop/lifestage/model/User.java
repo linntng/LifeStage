91121d4c8ab0665a7f81ac1d2848daa027f44b7f
oskarjansson <ogjanne@gmail.com>
2026-03-03 15:27:12 +0100

    feat: added getters and setters to user

--- backend/lifestage/src/main/java/com/loop/lifestage/model/User.java
+++ backend/lifestage/src/main/java/com/loop/lifestage/model/User.java
@@ -4,6 +4,7 @@ package com.loop.lifestage.model;
 import jakarta.persistence.*;
 
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
@@ -13,4 +14,27 @@ public class User {
     @Column(nullable = false)
     private String username;
 
+    public User() {
+    }   
+
+    public User(String id, String username) {
+        this.id = id;
+        this.username = username;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
