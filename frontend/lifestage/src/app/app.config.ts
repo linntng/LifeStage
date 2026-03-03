189634a2d96714dee5d8d04452b61f17883d4730
MartinHatlem <martinfhatlem@gmail.com>
2026-03-03 13:11:26 +0100

    chore: add keycloak config

--- frontend/lifestage/src/app/app.config.ts
+++ frontend/lifestage/src/app/app.config.ts
@@ -1,8 +1,23 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideKeycloak } from 'keycloak-angular';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-	providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+	providers: [
+		provideKeycloak({
+			config: {
+				url: 'https://lemur-9.cloud-iam.com/auth',
+				realm: 'lifestage',
+				clientId: 'lifestage',
+			},
+			initOptions: {
+				onLoad: 'check-sso',
+				silentCheckSsoRedirectUri: window.location.origin + '/silent-check-sso.html',
+			},
+		}),
+		provideBrowserGlobalErrorListeners(),
+		provideRouter(routes),
+	],
 };
