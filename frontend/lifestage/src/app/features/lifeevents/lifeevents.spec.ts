25b47b22e3f4033e6aa36c63dbd3bed539edd82f
linntng <truclinn1998@gmail.com>
2026-03-03 13:56:56 +0100

    feat: add lifeevent component

---
+++ frontend/lifestage/src/app/features/lifeevents/lifeevents.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Lifeevents } from './lifeevents';
+
+describe('Lifeevents', () => {
+  let component: Lifeevents;
+  let fixture: ComponentFixture<Lifeevents>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Lifeevents]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(Lifeevents);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/lifestage/src/app/features/lifeevents/lifeevents.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lifeevents',
+  imports: [],
+  templateUrl: './lifeevents.html',
+  styleUrl: './lifeevents.css',
+})
+export class Lifeevents {
+
+}
