c582719d6e79550a18c1333e03d031f0b4ea2fce
oskarjansson <ogjanne@gmail.com>
2026-03-04 10:52:05 +0100

    feat: added lifeevent service

---
+++ backend/lifestage/src/main/java/com/loop/lifestage/service/LifeEventService.java
@@ -0,0 +1,29 @@
+
+package com.loop.lifestage.service;
+
+import com.loop.lifestage.dto.LifeEventDTO;
+import com.loop.lifestage.model.LifeEvent;
+import com.loop.lifestage.repository.LifeEventRepository;
+import com.loop.lifestage.mapper.LifeEventMapper;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class LifeEventService {
+
+    private final LifeEventRepository lifeEventRepository;
+    private final LifeEventMapper lifeEventMapper;
+
+    public LifeEventService(LifeEventRepository lifeEventRepository, LifeEventMapper lifeEventMapper) {
+        this.lifeEventRepository = lifeEventRepository;
+        this.lifeEventMapper = lifeEventMapper;
+    }
+
+    public List<LifeEventDTO> getAllLifeEvents() {
+        List<LifeEvent> lifeEvents = lifeEventRepository.findAll();
+        return lifeEvents.stream()
+                .map(lifeEventMapper::toLifeEventDTO)
+                .collect(Collectors.toList());
+    }
+}
