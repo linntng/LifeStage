2a825848305e5895f4ee18c2a29d27612fba105e
oskarjansson <ogjanne@gmail.com>
2026-03-04 10:37:38 +0100

    feat: added lifeevent repository

---
+++ backend/lifestage/src/main/java/com/loop/lifestage/repository/LifeEventRepository.java
@@ -0,0 +1,8 @@
+
+package com.loop.lifestage.repository;
+
+import com.loop.lifestage.model.LifeEvent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LifeEventRepository extends JpaRepository<LifeEvent, Long> {
+}
