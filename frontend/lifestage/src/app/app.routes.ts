9a9726defc4379d0d44731ea7c46034203a755fc
linntng <truclinn1998@gmail.com>
2026-03-04 09:01:05 +0100

    feat: add routing to lifevent

--- frontend/lifestage/src/app/app.routes.ts
+++ frontend/lifestage/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: 'lifeevents',
+		loadComponent: () =>
+			import('./features/lifeevents/ui/lifeevents').then((l) => l.Lifeevents),
+	},
+];
