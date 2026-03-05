a0cdbadc15177bcd8af7eda4bacb7b7fb6cbfe36
MartinHatlem <martinfhatlem@gmail.com>
2026-03-05 10:12:30 +0100

    feat: add auth guard and 404 page

--- frontend/lifestage/src/app/app.routes.ts
+++ frontend/lifestage/src/app/app.routes.ts
@@ -1,9 +1,23 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/authGuard/auth-guard';
+import { LoginButton } from './features/auth/login-button/login-button';
+import { Lifeevents } from './features/lifeevents/ui/lifeevents';
+import { NotFound } from './features/not-found/not-found';
 
 export const routes: Routes = [
+	{
+		path: '',
+		component: LoginButton,
+		pathMatch: 'full',
+	},
 	{
 		path: 'lifeevents',
 		loadComponent: () =>
 			import('./features/lifeevents/ui/lifeevents').then((l) => l.Lifeevents),
+		canActivate: [authGuard],
+	},
+	{
+		path: '**',
+		component: NotFound,
 	},
 ];
