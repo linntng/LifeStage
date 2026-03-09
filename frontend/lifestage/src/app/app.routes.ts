255a33bd2dd0c5d01fadada90b88581c2a6027bd
linntng <truclinn1998@gmail.com>
2026-03-09 15:32:08 +0100

    feat: add dashboard files

--- frontend/lifestage/src/app/app.routes.ts
+++ frontend/lifestage/src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
 import { authGuard } from './core/authGuard/auth-guard';
-import { LoginButton } from './features/auth/login-button/login-button';
 import { NotFound } from './features/not-found/not-found';
+import { Dashboard } from './features/dashboard/dashboard';
 
 export const routes: Routes = [
 	{
 		path: '',
-		component: LoginButton,
+		component: Dashboard,
 		pathMatch: 'full',
 	},
 	{
