5ff7cdacb660580cda4fc08ea51d4490a1311dc3
oskarjansson <ogjanne@gmail.com>
2026-03-09 15:32:25 +0100

    feat: made policy endpoint public

--- backend/lifestage/src/main/java/com/loop/lifestage/security/RequestFilter.java
+++ backend/lifestage/src/main/java/com/loop/lifestage/security/RequestFilter.java
@@ -16,7 +16,7 @@ public class RequestFilter {
 
     http.csrf(csrf -> csrf.disable())
         .authorizeHttpRequests(
-            auth -> auth.requestMatchers("/lifeevents").permitAll().anyRequest().authenticated())
+            auth -> auth.requestMatchers("/lifeevents", "/policies").permitAll().anyRequest().authenticated())
         .oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()));
 
     return http.build();
