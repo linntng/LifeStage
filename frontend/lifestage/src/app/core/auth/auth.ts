fbd37e13cb81e829fa1e7c300dfa5d492549bf56
linntng <truclinn1998@gmail.com>
2026-03-10 13:02:04 +0100

    feat: add keycloak signals and make a function to get keycloak name

--- frontend/lifestage/src/app/core/auth/auth.ts
+++ frontend/lifestage/src/app/core/auth/auth.ts
@@ -1,15 +1,27 @@
-import { computed, inject, Injectable } from '@angular/core';
+import { computed, effect, inject, Injectable, signal } from '@angular/core';
 import Keycloak, { KeycloakUserInfo } from 'keycloak-js';
 import { User } from '../../features/user/state/user-api';
+import { KEYCLOAK_EVENT_SIGNAL, KeycloakEventType } from 'keycloak-angular';
 
 @Injectable({
 	providedIn: 'root',
 })
 export class Auth {
-	private keycloak = inject(Keycloak);
+	protected readonly keycloak = inject(Keycloak);
 
 	readonly authenticated = computed(() => this.keycloak.authenticated ?? false);
 	readonly token = computed(() => this.keycloak.token);
+	keycloakName = signal('');
+
+	private readonly keycloakSignal = inject(KEYCLOAK_EVENT_SIGNAL);
+
+	constructor() {
+		this.getGivenName();
+
+		effect(() => {
+			this.keycloakSignal();
+		});
+	}
 
 	login() {
 		this.keycloak.login();
@@ -34,6 +46,22 @@ export class Auth {
 			})
 			.catch(() => {
 				console.error("Couldn't get user info from Keycloak");
+			});
+	}
+
+	getGivenName() {
+		this.keycloak
+			.loadUserInfo()
+			.then((userInfo) => {
+				console.log(userInfo);
+				if (userInfo['given_name']) {
+					this.keycloakName.set(userInfo['given_name']);
+				} else {
+					this.keycloakName.set('');
+				}
+			})
+			.catch(() => {
+				this.keycloakName.set('');
 			});
 	}
 
