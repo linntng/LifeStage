c05fd0717381a80be0a99b571430e3b6e6fd7a86
MartinHatlem <martinfhatlem@gmail.com>
2026-03-11 11:05:48 +0100

    chore(cases+lifestage): setup Eureka & FeignClient config

--- backend/cases/src/main/java/com/loop/cases/CasesApplication.java
+++ backend/cases/src/main/java/com/loop/cases/CasesApplication.java
@@ -2,8 +2,12 @@ package com.loop.cases;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableEurekaServer
+@EnableFeignClients
 public class CasesApplication {
 
   public static void main(String[] args) {
--- backend/cases/src/main/java/com/loop/cases/service/PolicyCaseService.java
+++ backend/cases/src/main/java/com/loop/cases/service/PolicyCaseService.java
@@ -1,6 +1,5 @@
 package com.loop.cases.service;
 
-import java.util.List;
 import java.util.Set;
 
 import org.springframework.stereotype.Service;
@@ -13,18 +12,16 @@ import com.loop.cases.repository.PolicyCaseRepository;
 import java.util.stream.Collectors;
 
 import jakarta.persistence.EntityNotFoundException;
-import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Schedulers;
 
 @Service
 public class PolicyCaseService {
     
     private final PolicyCaseRepository policyCaseRepository;
-    private final UserRetrieverService userRetrieverService;
     
-    public PolicyCaseService(PolicyCaseRepository policyCaseRepository, UserRetrieverService userRetrieverService) {
+    public PolicyCaseService(
+        PolicyCaseRepository policyCaseRepository
+        ) {
         this.policyCaseRepository = policyCaseRepository;
-        this.userRetrieverService = userRetrieverService;
     }
 
     @Transactional
@@ -41,24 +38,12 @@ public class PolicyCaseService {
     }
 
     @Transactional(readOnly = true)
-    public Mono<Set<PolicyCase>> getAllPolicyCases(String userId, String token) {
+    public Set<PolicyCase> getAllPolicyCases(String userId, String token) {
         try {
-            return validateUserRole("CASE_HANDLER", userId, token)
-                .subscribeOn(Schedulers.boundedElastic()) // Create a separate thread for performance when calling external service
-                .then(Mono.just(policyCaseRepository.findAll().stream().collect(Collectors.toSet())));
+            // TODO: Validate roles and permissions using the token and userId
+            return policyCaseRepository.findAll().stream().collect(Collectors.toSet());
         } catch (Exception e) {
             throw new RuntimeException("An error occurred while retrieving users", e);
         }
-    }
-    
-    private Mono<Void> validateUserRole(String requiredRole, String userId, String token) {
-        return userRetrieverService.getUserRole(userId, token)
-            .flatMap(userRole -> {
-                 if (!requiredRole.equals(userRole)) {
-                    return Mono.error(new BadRequestException("User does not have the required role: " + requiredRole));
-                }
-                return Mono.empty();
-            }
-        );
     }
 }
