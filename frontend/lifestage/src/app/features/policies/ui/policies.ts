cea43c098a8219f702d58cac96bb194888682cb7
linntng <truclinn1998@gmail.com>
2026-03-11 10:05:31 +0100

    feat: add info card to policies page

--- frontend/lifestage/src/app/features/policies/ui/policies.ts
+++ frontend/lifestage/src/app/features/policies/ui/policies.ts
@@ -1,16 +1,18 @@
 import { Component, computed, inject } from '@angular/core';
 import { UserStore } from '../../user/state/user-store';
 import { PoliciesStore } from '../state/policies-store';
+import { InfoCard } from './info-card/info-card';
 
 @Component({
 	selector: 'app-policy',
-	imports: [],
+	imports: [InfoCard],
 	templateUrl: './policies.html',
 })
 export class Policies {
 	user = inject(UserStore);
 	userPolicies = this.user.userPolicies;
 	policiesStore = inject(PoliciesStore);
+	policies = this.policiesStore.policies;
 
 	remainingPolicies = computed(() => {
 		const policyIds = new Set(this.user.currentUser()?.policyIds || []);
