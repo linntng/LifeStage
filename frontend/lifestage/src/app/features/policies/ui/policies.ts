27058ab154b8d7861f6c25835948c17a9a2b58e9
linntng <truclinn1998@gmail.com>
2026-03-11 08:41:18 +0100

    feat: add policies ts file

--- frontend/lifestage/src/app/features/policies/ui/policies.ts
+++ frontend/lifestage/src/app/features/policies/ui/policies.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { UserStore } from '../../user/state/user-store';
+import { PoliciesStore } from '../state/policies-store';
 
 @Component({
 	selector: 'app-policy',
 	imports: [],
 	templateUrl: './policies.html',
 })
-export class Policies {}
+export class Policies {
+	user = inject(UserStore);
+	userPolicies = this.user.userPolicies;
+	policiesStore = inject(PoliciesStore);
+
+	remainingPolicies = computed(() => {
+		const policyIds = new Set(this.user.currentUser()?.policyIds || []);
+		return this.policiesStore.policies()?.filter((policy) => !policyIds.has(policy.id));
+	});
+
+	addPolicy() {}
+}
