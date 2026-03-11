75b68793c074116166b391444a7a728abefd9304
linntng <truclinn1998@gmail.com>
2026-03-11 14:13:51 +0100

    feat: add dialog and function to get lifeeventname from id

--- frontend/lifestage/src/app/features/policies/ui/info-card/info-card.ts
+++ frontend/lifestage/src/app/features/policies/ui/info-card/info-card.ts
@@ -1,19 +1,34 @@
 import { Component, inject, Input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { Policy } from '../../state/policies-api';
-import { PoliciesStore } from '../../state/policies-store';
-import { Auth } from '../../../../core/auth/auth';
-import { UserStore } from '../../../user/state/user-store';
+import { LifeeventStore } from '../../../lifeevents/state/lifeevent-store';
+import { MatDialog } from '@angular/material/dialog';
+import { PolicyDialog } from '../policy-dialog/policy-dialog';
+import { CapitalizePipe } from '../../../../shared/capitalize-pipe';
 
 @Component({
 	selector: 'app-info-card',
-	imports: [MatCardModule],
+	imports: [MatCardModule, CapitalizePipe],
 	templateUrl: './info-card.html',
 })
 export class InfoCard {
 	@Input() policy!: Policy;
 
-	policyStore = inject(PoliciesStore);
-	userStore = inject(UserStore);
-	auth = inject(Auth);
+	private dialog = inject(MatDialog);
+
+	lifeeventStore = inject(LifeeventStore);
+
+	getLifeeventName(id: number) {
+		return this.lifeeventStore.lifeevents()?.find((l) => l.id === id);
+	}
+
+	openDialog() {
+		const dialogRef = this.dialog.open(PolicyDialog, { data: this.policy });
+
+		dialogRef.afterClosed().subscribe((confirmed) => {
+			if (confirmed) {
+				console.log('Nice :)');
+			}
+		});
+	}
 }
