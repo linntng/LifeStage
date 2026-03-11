6b4646ccfd7ad1a6431676fd9d2d7ef0c02e9b09
linntng <truclinn1998@gmail.com>
2026-03-11 08:38:48 +0100

    feat: add loadpolicies

--- frontend/lifestage/src/app/app.ts
+++ frontend/lifestage/src/app/app.ts
@@ -2,6 +2,7 @@ import { Component, inject, signal, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LifeeventStore } from './features/lifeevents/state/lifeevent-store';
 import { Navbar } from './core/component/navbar/navbar';
+import { PoliciesStore } from './features/policies/state/policies-store';
 
 @Component({
 	selector: 'app-root',
@@ -12,8 +13,10 @@ import { Navbar } from './core/component/navbar/navbar';
 export class App implements OnInit {
 	protected readonly title = signal('lifestage');
 	lifeeventStore = inject(LifeeventStore);
+	policyStore = inject(PoliciesStore);
 
 	ngOnInit() {
 		this.lifeeventStore.loadLifeevents();
+		this.policyStore.loadPolicies();
 	}
 }
