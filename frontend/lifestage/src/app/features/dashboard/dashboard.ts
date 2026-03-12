f6b97878811b90660ae3e83a46ca1c2751217b18
linntng <truclinn1998@gmail.com>
2026-03-12 11:00:20 +0100

    feat: add users policies to the dashboard

--- frontend/lifestage/src/app/features/dashboard/dashboard.ts
+++ frontend/lifestage/src/app/features/dashboard/dashboard.ts
@@ -2,10 +2,11 @@ import { Component, inject, signal } from '@angular/core';
 import { UserStore } from '../user/state/user-store';
 import Keycloak from 'keycloak-js';
 import { Auth } from '../../core/auth/auth';
+import { CapitalizePipe } from '../../shared/capitalize-pipe';
 
 @Component({
 	selector: 'app-dashboard',
-	imports: [],
+	imports: [CapitalizePipe],
 	templateUrl: './dashboard.html',
 })
 export class Dashboard {
@@ -13,6 +14,7 @@ export class Dashboard {
 	user = this.userstore.currentUser;
 	auth = inject(Auth);
 	userLifeevents = this.userstore.userLifeevents;
+	userPolicies = this.userstore.userPolicies;
 
 	ngOnInit() {
 		console.log(this.auth.token);
