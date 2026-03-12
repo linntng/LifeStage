b8b51cb98469eb33823c35856ff60405d8d90a2b
MartinHatlem <martinfhatlem@gmail.com>
2026-03-12 13:19:14 +0100

    style: add standard table styling and case tables

--- frontend/lifestage/src/app/features/cases/ui/policy-cases.ts
+++ frontend/lifestage/src/app/features/cases/ui/policy-cases.ts
@@ -1,5 +1,6 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, computed, inject, OnInit } from '@angular/core';
 import { CaseStore } from '../state/case-store';
+import { PolicyCaseStatus } from '../state/policy-case-status';
 
 @Component({
 	selector: 'app-policy-cases',
@@ -9,9 +10,19 @@ import { CaseStore } from '../state/case-store';
 export class PolicyCases {
 	policyCaseStore = inject(CaseStore);
 	cases = this.policyCaseStore.cases;
+	activeCases = computed(() => this.cases()?.filter(c => c.status === PolicyCaseStatus.IN_REVIEW));
+	inactiveCases = computed(() => this.cases()?.filter(c => c.status !== PolicyCaseStatus.IN_REVIEW));
 
 	ngOnInit() {
 		this.policyCaseStore.loadAllCases();
+	}
+
+	addCase() {
+		this.policyCaseStore.addPolicyCaseToUser({
+			userId: "test-user-id2",
+			policyId: 0,
+			status: PolicyCaseStatus.IN_REVIEW,
+		});
 	}
 
 }
