474a3a13c7f0d35a7bbaeb3faa1a39c183215f9b
linntng <truclinn1998@gmail.com>
2026-03-20 14:21:53 +0100

    feat: add auditing to controllers

--- backend/lifestage/src/main/java/com/loop/lifestage/controller/PolicyController.java
+++ backend/lifestage/src/main/java/com/loop/lifestage/controller/PolicyController.java
@@ -1,8 +1,7 @@
 package com.loop.lifestage.controller;
 
-import com.loop.lifestage.dto.PolicyDTO;
-import com.loop.lifestage.service.PolicyService;
 import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -13,18 +12,28 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.loop.lifestage.dto.PolicyDTO;
+import com.loop.lifestage.service.AuditService;
+import com.loop.lifestage.service.PolicyService;
+
 @RestController
 @RequestMapping("/policies")
 public class PolicyController {
 
   private final PolicyService policyService;
+  private final AuditService auditService;
 
-  public PolicyController(PolicyService policyService) {
+  public PolicyController(PolicyService policyService, AuditService auditService) {
     this.policyService = policyService;
+    this.auditService = auditService;
   }
 
   @GetMapping("")
   public ResponseEntity<List<PolicyDTO>> getAllPolicies() {
+    auditService.logAccess(
+      "READ_LIST", 
+      "Policies"
+    );
     return ResponseEntity.ok(policyService.getAllPolicies());
   }
 
