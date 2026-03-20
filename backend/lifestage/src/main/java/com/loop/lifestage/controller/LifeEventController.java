c51f4be986d7c32aaad4f8b989d1146712ed3edf
linntng <truclinn1998@gmail.com>
2026-03-20 14:23:17 +0100

    feat: add auditing to controllers

--- backend/lifestage/src/main/java/com/loop/lifestage/controller/LifeEventController.java
+++ backend/lifestage/src/main/java/com/loop/lifestage/controller/LifeEventController.java
@@ -1,24 +1,33 @@
 package com.loop.lifestage.controller;
 
-import com.loop.lifestage.dto.LifeEventDTO;
-import com.loop.lifestage.service.LifeEventService;
 import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.loop.lifestage.dto.LifeEventDTO;
+import com.loop.lifestage.service.AuditService;
+import com.loop.lifestage.service.LifeEventService;
+
 @RestController
 @RequestMapping("/lifeevents")
 public class LifeEventController {
   private final LifeEventService lifeEventService;
+  private final AuditService auditService;
 
-  public LifeEventController(LifeEventService lifeEventService) {
+  public LifeEventController(LifeEventService lifeEventService, AuditService auditService) {
     this.lifeEventService = lifeEventService;
+    this.auditService = auditService;
   }
 
   @GetMapping
   public ResponseEntity<List<LifeEventDTO>> getAllLifeEvents() {
+    auditService.logAccess( 
+      "READ_LIST", 
+      "Life_event"
+    );
     return ResponseEntity.ok(lifeEventService.getAllLifeEvents());
   }
 }
