fc621517c5b762d9b24b73fe16853f109ff5f194
linntng <truclinn1998@gmail.com>
2026-03-23 15:41:31 +0100

    feat:linting

--- frontend/lifestage/src/app/features/policy-manager/policy-manager.ts
+++ frontend/lifestage/src/app/features/policy-manager/policy-manager.ts
@@ -1,31 +1,35 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject, OnInit } from '@angular/core';
 import { PoliciesStore } from '../policies/state/policies-store';
 import { PolicyStatus } from '../policies/state/policy-status.enum';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { CreatePolicyDialog } from './policy-dialogs/create-policy-dialog/create-policy-dialog';
 import { Policy } from '../policies/state/policies-api';
 import { ReviewPolicyDialog } from './policy-dialogs/review-policy-dialog/review-policy-dialog';
+import { LifeeventStore } from '../lifeevents/state/lifeevent-store';
 
 @Component({
 	selector: 'app-policy-manager',
-	imports: [MatDialogModule, CreatePolicyDialog],
+	imports: [MatDialogModule],
 	templateUrl: './policy-manager.html',
 })
-export class PolicyManager {
+export class PolicyManager implements OnInit {
 	policyStore = inject(PoliciesStore);
+	lifeeventStore = inject(LifeeventStore);
 	dialog = inject(MatDialog);
 	tab: 'active' | 'review' | 'rejected' = 'active';
 	selectedRejectedPolicy: any = null;
 
+	ngOnInit(): void {
+		this.policyStore.loadPolicies();
+		this.lifeeventStore.loadLifeevents();
+	}
 	getActivePolicies() {
 		return this.policyStore.getPoliciesFilteredByStatus([PolicyStatus.ACTIVE]);
 	}
 
-	getReviewPolicies() {
-		return this.policyStore.policiesToReview();
-	}
+	reviewPolicies = this.policyStore.policiesToReview;
 
-	getRejectedPoliciesWithExplanation() {
+	rejectedPoliciesWithExplanation = computed(() => {
 		const rejected = this.policyStore.policiesRejected();
 		const all = this.policyStore.policies();
 		if (rejected) {
@@ -44,7 +48,7 @@ export class PolicyManager {
 		} else {
 			return [];
 		}
-	}
+	});
 
 	createOrUpdatePolicyDialog(policy?: Policy) {
 		const dialogRef = this.dialog.open(CreatePolicyDialog, {
